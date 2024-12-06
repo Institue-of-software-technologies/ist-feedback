66ff619033b9d5db8605fae415e54d6910bdfe3b
James-Kabz <kabogp@gmail.com>
2024-12-06 22:27:07 +0300

    made the description to only appear in rating 4,3,2,1

--- src/app/student-feedback/[feedbackId]/page.tsx
+++ src/app/student-feedback/[feedbackId]/page.tsx
@@ -176,7 +176,6 @@ export default function StudentFeedback() {
                                     );
                                 })}
 
-
                                 {question.feedbackQuestion.questionType === "rating" && (
                                     <div className="mt-4">
                                         <div className="flex justify-center items-center overflow-x-auto">
@@ -185,9 +184,9 @@ export default function StudentFeedback() {
                                                     key={number}
                                                     type="button"
                                                     className={`h-10 w-14 md:h-10 md:w-32 sm:h-12 sm:w-32 lg:h-14 lg:w-28 inline-flex items-center justify-center text-base lg:text-xl font-medium border border-gray-300 
-                        ${watch(questionKey) === number ? "bg-red-600 text-white" : "bg-white text-gray-800"} 
-                        focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 
-                        hover:bg-red-600 hover:text-white whitespace-nowrap`}
+                    ${watch(questionKey) === number ? "bg-red-600 text-white" : "bg-white text-gray-800"} 
+                    focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 
+                    hover:bg-red-600 hover:text-white whitespace-nowrap`}
                                                     onClick={() => setValue(questionKey, number)}
                                                 >
                                                     {number}
@@ -195,23 +194,26 @@ export default function StudentFeedback() {
                                             ))}
                                         </div>
 
-                                        {/* Description field for rating */}
-                                        <div className="mt-4">
-                                            <textarea
-                                                rows={3}
-                                                id={`description-${question.feedbackQuestion.id}`}
-                                                placeholder="Add your description"
-                                                className="w-full p-2 border rounded"
-                                                {...register(`description-${question.feedbackQuestion.id}`,)}
-                                            />
-                                            {errors[`description-${question.feedbackQuestion.id}`] && (
-                                                <p className="text-red-500 text-sm mt-1">
-                                                    {errors[`description-${question.feedbackQuestion.id}`]?.message}
-                                                </p>
-                                            )}
-                                        </div>
+                                        {/* render the description input if the uder selects 4,3,2,1*/}
+                                        {(watch(questionKey) === 4 || watch(questionKey) === 3 || watch(questionKey) === 2 || watch(questionKey) === 1) && (
+                                            <div className="mt-4">
+                                                <textarea
+                                                    rows={3}
+                                                    id={`description-${question.feedbackQuestion.id}`}
+                                                    placeholder="Add your description"
+                                                    className="w-full p-2 border rounded"
+                                                    {...register(`description-${question.feedbackQuestion.id}`)}
+                                                />
+                                                {errors[`description-${question.feedbackQuestion.id}`] && (
+                                                    <p className="text-red-500 text-sm mt-1">
+                                                        {errors[`description-${question.feedbackQuestion.id}`]?.message}
+                                                    </p>
+                                                )}
+                                            </div>
+                                        )}
                                     </div>
                                 )}
+
 
 
                                 {errors[questionKey] && (
